a4d47360f71ab3cd716e7b48f3951ba84525a9af
Almujtaba <mujtabaimad8@gmail.com>
2024-06-10 19:55:24 +0400

    ref: cleaned initial project

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,18 @@
-import { ReportHandler } from 'web-vitals';
+import { Metric } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+
+const logReport = (metric: Metric) => {
+  console.log(`${metric.name} => value:${metric.value.toLocaleString()}, delta:${metric.delta.toLocaleString()}`)
+}
+
+const reportWebVitals = () => {
+  import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    getCLS(logReport);
+    getFID(logReport);
+    getFCP(logReport);
+    getLCP(logReport);
+    getTTFB(logReport);
+  });
 };
 
 export default reportWebVitals;
